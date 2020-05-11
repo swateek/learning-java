500ad5722ada4de7a08b0b0f38d182b3e78f0373
Swateek Jena <swateek@jena.com>
2020-05-12 00:22:46 +0530

    adding missed implementations

--- CoreJavaSnippets/Classes/StaticInnerClassExample.java
+++ CoreJavaSnippets/Classes/StaticInnerClassExample.java
@@ -2,6 +2,10 @@ package CoreJavaSnippets.Classes;
 
 
 public class StaticInnerClassExample {
+
+    int nonStaticVal = 10;
+    static int staticVal = 20;
+
     static private class InnerStatic {
         String innerVal;
 
@@ -13,14 +17,26 @@ public class StaticInnerClassExample {
             this.innerVal = innerVal;
         }
 
+        public String msg() {
+            // return "The outer class's value is " + nonStaticVal; // cannot access non-static 
+            return "The outer class's value is " + staticVal;
+        }
+
         @Override
         public String toString(){
             return getInnerVal();
         }
-
     }
 
     public static void main(String[] args){
-        
+        //StaticInnerClassExample sic = new StaticInnerClassExample();
+        //InnerStatic is = sic.new InnerStatic(); // won't work.
+        InnerStatic is = new StaticInnerClassExample.InnerStatic();
+
+        is.setInnerVal("Hello, Inner!");
+        System.out.println(is.toString()); // Hello, Inner!
+
+        System.out.println(is.msg()); // The outer class's value is 20
+
     }
 }
